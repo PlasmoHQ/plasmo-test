c07ec4fbbf34abcc1224d8a2e8bfdce804b2cfa8
Louis <6723574+louisgv@users.noreply.github.com>
2022-10-18 06:27:03 -0400

    adding sandbox pages example

---
+++ rfc/rfc-263-sandbox-pages/popup.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react"
+
+function IndexPopup() {
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      console.log("EVAL output: " + event.data)
+    })
+  }, [])
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      <button
+        onClick={() => {
+          iframeRef.current.contentWindow.postMessage("10 + 20", "*")
+        }}>
+        Trigger iframe eval
+      </button>
+      <iframe src="sandbox.html" ref={iframeRef} style={{ display: "none" }} />
+    </div>
+  )
+}
+
+export default IndexPopup
---
+++ rfc/rfc-263-sandbox-pages/sandboxes/test.tsx
@@ -0,0 +1,14 @@
+function TestSandbox() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      HELLO SANDBOX
+    </div>
+  )
+}
+
+export default TestSandbox
